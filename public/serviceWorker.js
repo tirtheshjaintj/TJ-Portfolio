352189383273fba7588197d983d779b511797966
tj <tirtheshjaintj@gmail.com>
2024-04-11 10:50:13 +0530

    Some Changes in Dark Mode

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -14,6 +14,7 @@ self.addEventListener('install', function (event) {
             })
     );
 });
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(caches.match(event.request)
         .then(function (response) {
@@ -35,3 +36,18 @@ self.addEventListener('install', function (event) {
     );
     self.skipWaiting();
 });
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+            return cacheName;
+          }).map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
+  
