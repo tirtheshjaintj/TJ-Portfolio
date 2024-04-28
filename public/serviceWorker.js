6857f792924ca3a70cd17a819422bebae5903481
tj <tirtheshjaintj@gmail.com>
2024-04-28 11:21:23 +0530

    Offline Mode Added

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,64 +1,63 @@
-let CACHE_NAME = 'my-site-cache-v1';
+const CACHE_NAME = 'my-site-cache-v1';
 
 const urlsToCache = [
-"static/",
-"index.html",
-"bundle.js",
-"/tj.png",
-"/avatar.svg",
-"/basee.svg",
-"/manifest.json"];
+  '/',
+  '/index.html',
+  '/bundle.js',
+  '/tj.png',
+  '/avatar.svg',
+  '/basee.svg',
+  '/manifest.json',
+  '/avatar.svg'
+];
 
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
-      }
-      const response = await fetch(e.request);
-      const cache = await caches.open(CACHE_NAME);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })(),
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('install', function (event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-    self.skipWaiting();
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request)
+          .then(function (response) {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
 });
 
-// self.addEventListener('activate', function(event) {
-//     event.waitUntil(
-//       caches.keys().then(function(cacheNames) {
-//         return Promise.all(
-//           cacheNames.filter(function(cacheName) {
-//             return cacheName;
-//           }).map(function(cacheName) {
-//             return caches.delete(cacheName);
-//           })
-//         );
-//       })
-//     );
-//   });
-  
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
